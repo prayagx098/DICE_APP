cca0be8955356508f1aad42467489bc8f24f1f69
prayagx098 <prayagjishnu123@gmail.com>
2024-08-16 15:49:50 +0530

    updated ui

--- script.js
+++ script.js
@@ -3,41 +3,43 @@ const dice = document.getElementById('dice');
 const audio = new Audio('dice.mp3');
 
 
-// Function to show the dice faces
+function diceShow() {
 
-function diceShow(){
+    while (dice.firstChild) {
+        dice.removeChild(dice.firstChild);
+    }
 
-    for(let i = 1; i <= 6; i++){
-
+    for (let i = 1; i <= 6; i++) {
         const face = document.createElement('div');
         face.classList.add('face', `face${i}`);
         face.textContent = i;
         dice.appendChild(face);
-        
     }
-
 }
-
 
 window.onload = diceShow;
 
-Button.addEventListener('click', ()=>{
+Button.addEventListener('click', () => {
     diceRoll();
 });
 
-function diceRoll(){
-
-
+function diceRoll() {
     audio.play();
-
     dice.classList.add('roll');
-
 
     const rand = Math.floor(Math.random() * 6) + 1;
 
 
     setTimeout(() => {
         dice.classList.remove('roll');
-        dice.style.transform = `rotateX(${rand * 90}deg)`;
-    }, 600);
+        const rotations = {
+            1: 'rotateX(0deg) rotateY(0deg)',
+            2: 'rotateX(0deg) rotateY(-90deg)',
+            3: 'rotateX(0deg) rotateY(-180deg)',
+            4: 'rotateX(0deg) rotateY(-270deg)',
+            5: 'rotateX(-90deg) rotateY(0deg)',
+            6: 'rotateX(90deg) rotateY(0deg)'
+        };
+        dice.style.transform = rotations[rand];
+    }, 1000); 
 }
